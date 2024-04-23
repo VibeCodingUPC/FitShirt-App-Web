d728b42b951b8a7d21adcd8839974f1e2786a458
DiegoDefSan <defsan35@gmail.com>
2024-04-23 11:26:30 -0500

    feat: modified App.vue Added router-view, header, toolbar and footer

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,17 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import theHeader from './components/shared/the-header.component.vue'
+  import theFooter from './components/shared/the-footer.component.vue'
+  import theToolbar from './components/shared/the-toolbar.component.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
+  <the-header/>
   <main>
-    <TheWelcome />
+    <the-toolbar/>
+    <router-view></router-view>
   </main>
+  <the-footer/>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
