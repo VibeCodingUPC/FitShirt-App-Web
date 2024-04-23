05c2c974d72012b80db15b04fd190c585dd53014
DiegoDefSan <defsan35@gmail.com>
2024-04-23 11:24:00 -0500

    feat: added catalogue component Added catalogue components: - Shirt card: contain shirt information - Shirt filters: Catalogue filters - no methods implemented and no api service communication - Shirt catalogue

---
+++ src/components/catalogue/shirt-card.component.vue
@@ -0,0 +1,58 @@
+<script setup>
+
+  const props = defineProps({
+    id: {
+      type: Number,
+      required: true
+    },
+    imageSrc: {
+      type: String,
+      required: false
+    },
+    name: {
+      type: String,
+      required: true
+    },
+  })
+</script>
+
+<template>
+  <pv-card class="shirt-card">
+    <template #header>
+      <img 
+        :alt="$props.name + ' image'"
+        :src="$props.imageSrc"
+      />
+    </template>
+    <template #title>
+      <h1>
+        {{ props.name }}
+      </h1>
+    </template>
+  </pv-card>
+</template>
+
+<style scoped>
+  .shirt-card {
+    width: 180px;
+    height: 220px;
+    background-color: #D9D9D9;
+    border-radius: 6px;
+    padding: .6em;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+  }
+  .shirt-card img {
+    width: 100%;
+    height: 140px;
+    object-fit: contain;
+    border-radius: 8px;
+  }
+  .shirt-card h1 {
+    text-align: center;
+    font-size: 1.2em;
+    font-weight: 600;
+    margin: .4em 0
+  }
+</style>
---
+++ src/components/catalogue/shirt-filters.component.vue
@@ -0,0 +1,100 @@
+<script setup>
+import { ref } from "vue";
+
+const selectedCategory = ref("Any");
+const categories = ref([
+  {"id": 99,"name": "Any"},
+  {"id": 1,"name": "Football"},
+  {"id": 2,"name": "Volleyball"},
+  {"id": 3,"name": "Basketball"}
+]);
+
+const selectedColor = ref("Any");
+const colors = ref([
+  {"id": 99, "name": "Any"},
+  {"name": "Red"},
+  {"name": "Yellow"},
+  {"name": "Purple"},
+  {"name": "Gray"},
+  {"name": "White"},
+  {"name": "Black"},
+  {"name": "Green"},
+  {"name": "Blue"}
+]);
+</script>
+
+<template>
+  <div class="filter-container">
+    <form class="filter-form">
+      <div class="input-container">
+        <label for="category-input">  
+          Category: 
+        </label>
+        <select v-model="selectedCategory">
+          <option 
+            v-for="category in categories" 
+            :value="category.name" 
+            :key="category.id"
+            id="category-input">
+            {{ category.name }}
+          </option>
+        </select>
+      </div>
+      <div class="input-container">
+        <label for="color-input">Color: </label>
+        <select v-model="selectedColor">
+          <option 
+            v-for="color in colors" 
+            :value="color.name" 
+            :key="color.id"
+            id="color-input">
+            {{ color.name }}
+          </option>
+        </select>
+      </div>
+      <button class="form-button">
+        Filter
+      </button>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+.filter-container {
+  padding: .4em;
+}
+.filter-container .filter-form {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+}
+.filter-container .filter-form .input-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin: .4em;
+}
+.filter-container .filter-form .input-container label {
+  margin-right: .4em;
+}
+.filter-container .filter-form .input-container select {
+  align-items: center;
+  width: 140px;
+  font-size: .8rem;
+  padding: .3em;
+  border: solid black 1px;
+  border-radius: 6px;
+}
+.filter-container .filter-form .form-button {
+  background-color: #4D94FF;
+  padding: .8em 2.8em;
+  border-radius: 8px;
+  color: #E5E5E5;
+  font-size: .8em;
+  font-weight: 600;
+  cursor: pointer;
+}
+</style>
