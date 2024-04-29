c102c799f79ba2a6b02cd44905029c147be769df
Agustin <agustin.alejandro1012@hotmail.com>
2024-04-29 15:13:17 -0500

    Added new feature: ToolBar in Cart

--- src/components/cart/user-cart.component.vue
+++ src/components/cart/user-cart.component.vue
@@ -1,6 +1,7 @@
 <script setup>
   import { onMounted, ref } from 'vue';
   import {CartApiService} from "@/services/shirts-api.service.js";
+  import TheToolbar from "@/components/shared/the-toolbar.component.vue";
 
   let items=ref([]);
   const cartService = new CartApiService();
@@ -16,32 +17,37 @@
 </script>
 
 <template>
-  <div class="subitem-container">
-    <p>Subtotal:&nbsp </p>
-    <div class="info-container"></div>
-  <router-link to="/catalogue">
-    <pv_button class="button-style">Comprar</pv_button>
-  </router-link>
-  </div>
-  <div class="cart-container">
-    <div v-for="item in items" :key="item.id">
-      <div class="item-container">
-        <img :src="item.image" alt="Item-Image" class="image-container"/>
-        <p>&nbsp{{item.name}}&nbsp</p>
-        <div class="subitem-container">
-          <p>Cantidad:&nbsp</p>
-          <pv_inputText v-model="item.quantity" class="editableinfo-container"></pv_inputText>
-        </div>
-        <div class="subitem-container">
-          <p>Precio:&nbsp</p>
-          <div class="info-container">S/. {{item.price}}</div>
-        </div>
-        <div class="subitem-container">
-          <p>Subtotal:&nbsp </p>
-          <div class="info-container">S/. {{item.price*2}}</div>
-        </div>
-        <div>
-          <img src="/images/bin.png" alt="bin-Image" class="bin-container">
+  <div class="main-container">
+    <the-toolbar/>
+    <div class="cart-container">
+      <div class="header-container">
+        <p>Subtotal:&nbsp </p>
+        <div class="info-container"></div>
+        <router-link to="/catalogue">
+          <pv_button class="button-style">Comprar</pv_button>
+        </router-link>
+      </div>
+      <div v-for="item in items" :key="item.id">
+        <div class="item-container">
+          <div class="subitem-container">
+            <img :src="item.image" alt="Item-Image" class="image-container"/>
+            <p>&nbsp{{item.name}}&nbsp</p>
+          </div>
+          <div class="subitem-container">
+            <p>Cantidad:&nbsp</p>
+            <pv_inputText v-model="item.quantity" class="editableinfo-container"></pv_inputText>
+          </div>
+          <div class="subitem-container">
+            <p>Precio:&nbsp</p>
+            <div class="info-container">S/. {{item.price}}</div>
+          </div>
+          <div class="subitem-container">
+            <p>Subtotal:&nbsp </p>
+            <div class="info-container">S/. {{item.price*2}}</div>
+          </div>
+          <div>
+            <img src="/images/bin.png" alt="bin-Image" class="bin-container">
+          </div>
         </div>
       </div>
     </div>
@@ -49,6 +55,12 @@
 </template>
 
 <style scoped>
+.main-container{
+  flex-direction: row;
+  width: auto;
+  display: flex;
+  flex-wrap: wrap;
+}
 .subitem-container{
   background-color: #cacaca;
   padding: 20px;
@@ -62,6 +74,15 @@
 .bin-container{
   height: 20px;
   width: 20px;
+}
+.header-container{
+  padding: 20px;
+  flex-direction: row;
+  width: auto;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
 }
 .item-container{
   background-color: #cacaca;
