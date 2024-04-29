e69405be6c35b8424d30652e9dbf580cac24fde4
Agustin <agustin.alejandro1012@hotmail.com>
2024-04-29 15:13:17 -0500

    Upload: New icon Images

--- src/components/shared/the-toolbar.component.vue
+++ src/components/shared/the-toolbar.component.vue
@@ -2,8 +2,40 @@
 </script>
 
 <template>
+  <div class="icons-container">
+    <router-link to="/profile">
+      <div>
+        <img src="/icons/ProfileIcon.png" alt="bin-Image" class="icon-container"></div></router-link>
+    <router-link to="/catalogue">
+      <div>
+        <img src="/icons/CatalogoIcon.png" alt="bin-Image" class="icon-container"></div></router-link>
+    <router-link to="/">
+      <div>
+        <img src="/icons/ShopIcon.png" alt="bin-Image" class="icon-container"></div></router-link>
+    <router-link to="/">
+      <div>
+        <img src="/icons/DesignIcon.png" alt="bin-Image" class="icon-container"></div></router-link>
+    <router-link to="/cart">
+      <div>
+        <img src="/icons/CartIcon.png" alt="bin-Image" class="icon-container"></div></router-link>
+    <router-link to="/">
+      <div>
+        <img src="/icons/OremiumIcon.png" alt="bin-Image" class="icon-container"></div></router-link>
 
+
+
+
+
+
+  </div>
 </template>
 
 <style scoped>
+.icons-container{
+  background-color: #cacaca;
+}
+.icon-container{
+  height: 60px;
+  width: 60px;
+}
 </style>
