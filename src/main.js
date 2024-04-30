5631a62977d3587c7d7c4524eb6682d8843763a0
Agustin <agustin.alejandro1012@hotmail.com>
2024-04-29 19:39:04 -0500

    Revert "Added new feature: User Profile"

--- src/main.js
+++ src/main.js
@@ -1,20 +1,17 @@
 import './assets/main.css'
-import 'primevue/resources/themes/aura-light-blue/theme.css'
+
 import { createApp } from 'vue'
 import App from './App.vue';
 import router from './routes/router';
 import PrimeVue from 'primevue/config';
 
 import Card from 'primevue/card';
-import InputText from "primevue/inputtext";
-import Button from 'primevue/button';
+
 const app = createApp(App);
 
 app.use(PrimeVue);
 app.use(router);
 
-app.component('pv_card',Card);
-app.component('pv_inputText',InputText);
-app.component('pv_button',Button);
+app.component('pv-card', Card);
 
 app.mount('#app');
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,17 +1,8 @@
 import ShirtsCatalogueComponent from '@/components/catalogue/shirts-catalogue.component.vue';
-import UserProfileComponent from '@/components/profile/user-profile.component.vue';
-import EditProfileComponent from '@/components/profile/edit-profile.component.vue';
-import EditPaymentMethod from "@/components/profile/edit-payment-method.component.vue";
-import EditPassword from "@/components/profile/edit-password.component.vue";
 import {createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
-  { path: '/', component: UserProfileComponent },
-  { path: '/profile', component: UserProfileComponent },
-  { path: '/edit-profile', component: EditProfileComponent },
-  { path: '/edit-password', component: EditPassword },
-  { path: '/edit-payment-method', component: EditPaymentMethod },
-  { path: '/catalogue', component: ShirtsCatalogueComponent},
+  {path: '/catalogue', component: ShirtsCatalogueComponent},
 ];
 
 const router = createRouter({
