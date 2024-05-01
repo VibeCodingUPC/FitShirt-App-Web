4a6a4d112b77aeeab4361485e7e4d282dd416b65
Agustin <agustin.alejandro1012@hotmail.com>
2024-05-01 12:33:21 -0500

    update: User cart feature

--- src/services/shirts-api.service.js
+++ src/services/shirts-api.service.js
@@ -9,12 +9,3 @@ export class ShirtsApiService {
   }
 
 }
-export class CartApiService {
-
-  async getCarItems() {
-    let {getObjects} = useApi('/cart.data.json');
-
-    return await getObjects();
-  }
-
-}
