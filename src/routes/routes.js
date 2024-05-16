3afe48f870f2afd27ca69266e165993b8ab5bd39
DiegoDefSan <defsan35@gmail.com>
2024-05-15 23:24:59 -0500

    ref: refactored shopping cart component

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,12 +2,16 @@ import { catalogueRoutes } from "./modules/catalogue.routes.js";
 import { authenticationRoutes } from "./modules/authentication.routes.js";
 import { designRoutes } from "@/routes/modules/design.routes.js";
 import { postsRoutes } from "@/routes/modules/posts.routes.js";
-import {errorRoutes} from "@/routes/modules/error.routes.js";
+import { errorRoutes } from "@/routes/modules/error.routes.js";
+import UserLoginPage from "@/components/authentication/login/user-login.page.vue";
+import { shoppingCart } from "./modules/shopping-cart.routes.js";
 
 export default [
+  { path: '/', component: UserLoginPage },
   ...catalogueRoutes,
   ...authenticationRoutes,
   ...designRoutes,
   ...postsRoutes,
-  ...errorRoutes
+  ...errorRoutes,
+  ...shoppingCart
 ];
