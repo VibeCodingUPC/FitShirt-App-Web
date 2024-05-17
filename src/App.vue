7bdd6c07e4b628973f1ae5604a29259842aec16d
DiegoDefSan <defsan35@gmail.com>
2024-05-16 20:38:56 -0500

    ref: removed unused items

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,10 @@
 <script setup>
-  // import theToolbar from './components/shared/the-toolbar.component.vue';
 </script>
 
 <template>
   <router-view>
   </router-view>
-  <!-- <main class="main">
-    <the-toolbar/>
-  </main> -->
 </template>
 
 <style scoped>
-/* .main {
-  display: flex;
-  justify-content: space-around;
-  width: 85vw;
-  max-width: 1050px;
-  padding: .6em 1.2em;
-  margin: auto;
-  background-color: #f3f3f3;
-}
-@media (max-width: 600px) {
-  .main {
-    width: 100vw;
-  }
-} */
 </style>
