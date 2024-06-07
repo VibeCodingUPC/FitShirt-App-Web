4ddcfabd86c8efebedaf02c1da1cab147238f5aa
Diego Defilippi <defsan35@gmail.com>
2024-06-07 13:12:20 -0500

    feat: added base url

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const useAxios = () => {
-  const apiKey = "66339c32f7d50bbd9b4a0b1a";
-  const baseUrl = `http://localhost:3000`;
+  const baseUrl = `https://localhost:7058/api/v1`;
 
   const axiosInstance = axios.create({
     baseURL: baseUrl
