b7546960ab7f72111b015fdb84134dc8512724f8
Jhinta <raisotil7@hotmail.com>
2024-11-16 13:47:44 -0500

    feat: loader clients section

--- src/components/client-layout/catalogue/shirts-catalogue/shirts-catalogue.component.vue
+++ src/components/client-layout/catalogue/shirts-catalogue/shirts-catalogue.component.vue
@@ -4,16 +4,19 @@ import filters from './shirt-filters.component.vue';
 import shirtCard from '@/components/shared/shirt-components/shirt-card.component.vue';
 import { PostsApiService } from '@/services/posts-api.service';
 
-let shirts=ref([]);
+let shirts = ref([]);
+let isLoading = ref(true); // Estado para controlar el loader
 const postsService = new PostsApiService();
 
 const fetchShirtsData = async () => {
+  isLoading.value = true; // Mostrar loader al iniciar el fetch
   shirts.value = await postsService.getPosts();
-}
+  isLoading.value = false; // Ocultar loader al finalizar el fetch
+};
 
 onMounted(async () => {
   await fetchShirtsData();
-})
+});
 </script>
 
 <template>
@@ -21,7 +24,12 @@ onMounted(async () => {
     class="catalogue-container"
     aria-describedby="Interactive jerseys catalogue with filters to browse">
     <filters/>
-    <div class="shirts-container">
+    <!-- Mostrar loader mientras se cargan los datos -->
+    <div v-if="isLoading" class="loader-container">
+      <img src="/images/loanding.webp" alt="loader" class="loader" />
+    </div>
+    <!-- Mostrar las camisetas una vez cargadas -->
+    <div v-else class="shirts-container">
       <shirt-card
         v-for="shirt in shirts"
         :key="shirt.id"
@@ -50,4 +58,15 @@ onMounted(async () => {
     background-color: #e0e7ff;
   }
 
+  .loader-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 50vh; /* Ajustar según el diseño */
+  }
+
+  .loader {
+    width: 16vw;
+    height: 20vh;
+  }
 </style>
